21caf58092d1a920fa9ada33f4e1de5bba2e0a1d
SHIMOMURA Sho <graueule@gmail.com>
2013-04-05 08:59:12 +0900

    add resizing feature to notification area

--- app/assets/javascripts/notification_area.js
+++ app/assets/javascripts/notification_area.js
@@ -1,55 +1,52 @@
 var notificationArea = (function(){
+    function resizeSidebar(sidebarWidth){
+        var distance =
+            (document.body.clientWidth - (mainWidth + widthMargin)) / 2 - sidebarWidth;
+        if (distance > 0) {
+            distance = 0;
+        }
+
+        main.css({
+            "transform" : "translate("+distance+"px, 0px)",
+            "-webkit-transform" : "translate("+distance+"px, 0px)",
+            "-moz-transform" : "translate("+distance+"px, 0px)",
+        });
+        notificationArea.css("width", ""+sidebarWidth+"px");
+    }
+
     var sidebarWidth = 400;
     var mainWidth = 720;
     var widthMargin = 120;
 
     var notificationArea = $("#notification_area");
     var main = $(".main");
-    var button = $("#open_button");
+    var button = $("#open_button span");
 
     var menuClosed = true;
     button.bind("click", function(){
         if (menuClosed) {
-            var distance = (document.body.clientWidth - (mainWidth + widthMargin)) / 2 - sidebarWidth;
-            if (distance < 0) {
-                main.css({
-                    "transform" : "translate("+distance+"px, 0px)",
-                    "-webkit-transform" : "translate("+distance+"px, 0px)",
-                    "-moz-transform" : "translate("+distance+"px, 0px)",
-                });
-            }
-
-            notificationArea.css("width", ""+sidebarWidth+"px");
+            resizeSidebar(sidebarWidth);
             button.text("CLOSE");
             menuClosed = false;
         } else {
-            notificationArea.css("width", "0px");
-            main.css({
-                "transform" : "translate(0px, 0px)",
-                "-webkit-transform" : "translate(0px, 0px)",
-                "-moz-transform" : "translate(0px, 0px)",
-            });
+            resizeSidebar(0);
             button.text("OPEN");
             menuClosed = true;
         }
     });
 
-    var sections = {};
-    return ({
-        addSection : function(id, title){
-            if (sections[id]) {
-            } else {
-                var section = $("<div></div>").css({
-                    "margin-left" : "20px",
-                    "color" : "white",
-                });
-                var header = $("<h3></h3>").text(title);
-                section.append(header);
-                notificationArea.append(section);
-                sections[id] = section;
-                return section;
-            }
+    var pos_x = undefined;
+    $("body").bind("mouseup", function(){
+        pos_x = undefined;
+    }).bind("mousemove", function(e){
+        if (pos_x != undefined) {
+            sidebarWidth += pos_x - e.screenX;
+            pos_x = e.screenX;
+            resizeSidebar(sidebarWidth);
         }
     });
+    $("#notification_area #grip").bind("mousedown", function(e){
+        e.preventDefault();
+        pos_x = e.screenX;
+    });
 })();
-
