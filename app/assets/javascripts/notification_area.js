f247b290c719faa5f7c37f7eabfb159a8aa6ec75
SHIMOMURA Sho <graueule@gmail.com>
2013-07-06 14:25:52 +0900

    refactoring

--- app/assets/javascripts/notification_area.js
+++ app/assets/javascripts/notification_area.js
@@ -1,41 +1,22 @@
-(function(){
-    function resizeSidebar(width){
-        var distance =
-            (document.body.clientWidth - (mainWidth + widthMargin)) / 2 - width;
+var notificationArea = (function(){
+    function closeSidebar() {
+        resizeSidebar(0, 0);
+    }
+    function openSidebar() {
+        var width = sidebarInfo.width;
+        var distance = (document.body.clientWidth - (mainWidth + widthMargin)) / 2 - width;
         if (distance > 0) {
             distance = 0;
         }
-
+        resizeSidebar(distance, width);
+    }
+    function resizeSidebar(distance, width){
         main.css({
             "transform" : "translate("+distance+"px, 0px)",
             "-webkit-transform" : "translate("+distance+"px, 0px)",
             "-moz-transform" : "translate("+distance+"px, 0px)",
         });
         notificationArea.css("width", ""+width+"px");
-    }
-    function enableAnimation(){
-        $("#notification_area").css({
-            "transition" : "width 0.1s ease-out",
-            "-webkit-transition" : "width 0.1s ease-out",
-            "-moz-transition" : "width 0.1s ease-out",
-        });
-        $(".main").css({
-            "transition" : "transform 0.1s ease-out",
-            "-webkit-transition" : "-webkit-transform 0.1s ease-out",
-            "-moz-transition" : "-moz-transform 0.1s ease-out",
-        });
-    }
-    function disableAnimation(){
-        $("#notification_area").css({
-            "transition" : "none",
-            "-webkit-transition" : "none",
-            "-moz-transition" : "none",
-        });
-        $(".main").css({
-            "transition" : "none",
-            "-webkit-transition" : "none",
-            "-moz-transition" : "none",
-        });
     }
 
     var sidebarInfo = $.LocalStorage.get("sidebarInfo", {
@@ -51,38 +32,56 @@
 
     button.bind("click", function(){
         if (sidebarInfo.opened) {
-            resizeSidebar(0);
+            closeSidebar();
             button.text("OPEN");
             sidebarInfo.opened = false;
         } else {
-            resizeSidebar(sidebarInfo.width);
+            openSidebar();
             button.text("CLOSE");
             sidebarInfo.opened = true;
         }
         $.LocalStorage.set("sidebarInfo", sidebarInfo);
     });
     if (sidebarInfo.opened) {
-        resizeSidebar(sidebarInfo.width);
+        openSidebar();
         button.text("CLOSE");
     }
 
     var pos_x = undefined;
     $("body").bind("mouseup", function(){
         if (pos_x != undefined) {
-            pos_x = undefined;
-            $.LocalStorage.set("sidebarInfo", sidebarInfo);
-            enableAnimation();
+           pos_x = undefined;
+           $.LocalStorage.set("sidebarInfo", sidebarInfo);
+$("#notification_area").css({
+   "transition" : "width 0.1s ease-out",
+   "-webkit-transition" : "width 0.1s ease-out",
+   "-moz-transition" : "width 0.1s ease-out",
+});
+$(".main").css({
+   "transition" : "transform 0.1s ease-out",
+   "-webkit-transition" : "-webkit-transform 0.1s ease-out",
+   "-moz-transition" : "-moz-transform 0.1s ease-out",
+});
         }
     }).bind("mousemove", function(e){
         if (pos_x != undefined) {
             sidebarInfo.width += pos_x - e.screenX;
             pos_x = e.screenX;
-            resizeSidebar(sidebarInfo.width);
+            openSidebar();
         }
     });
     $("#notification_area #grip").bind("mousedown", function(e){
         e.preventDefault();
         pos_x = e.screenX;
-        disableAnimation();
+$("#notification_area").css({
+   "transition" : "none",
+   "-webkit-transition" : "none",
+   "-moz-transition" : "none",
+});
+$(".main").css({
+   "transition" : "none",
+   "-webkit-transition" : "none",
+   "-moz-transition" : "none",
+});
     });
 })();
